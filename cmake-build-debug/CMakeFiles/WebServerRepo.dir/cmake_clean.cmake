eb10e10a8c6939f77fe4c2212fe4e9bb4ee8b25b
Abdo Elfaramawy <elfar009@umn.edu>
2018-03-12 23:40:15 -0500

    Added project to Clion for easier debugging

---
+++ cmake-build-debug/CMakeFiles/WebServerRepo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WebServerRepo.dir/main.c.o"
+  "WebServerRepo.pdb"
+  "WebServerRepo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WebServerRepo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
